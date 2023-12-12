d84bdfc8bf6efdb42d7ad6ae9d31c1d71f66cfdf
Frnkp21 <perezcarvajalfrank@elpuig.xeill.net>
2023-12-12 11:06:08 +0100

    BE_Frank_12_12_23:BBDD modificada + novedades

--- BBDD_DEFINITIVA/crear+inserts.sql
+++ BBDD_DEFINITIVA/crear+inserts.sql
@@ -97,13 +97,13 @@ CREATE TABLE federation_rol (
 CREATE TABLE novedades (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(45),
-    gim_name VARCHAR(45),
+    gym_name VARCHAR(45),
     fecha DATE,
-    informacion VARCHAR(200);
+    informacion VARCHAR(200)
 );
 
 
-INSERT INTO novedades (titulo, gim_name, fecha, informacion) VALUES
+INSERT INTO novedades (titulo, gym_name, fecha, informacion) VALUES
 ('Entrenamiento de alta intensidad', 'GymFit', '2023-01-15', '¡Descubre nuestra nueva clase de alta intensidad para todos los niveles!'),
 ('Apertura de nuevas sucursales', 'PowerGym', '2023-02-02', 'Celebra con nosotros la apertura de nuevas sucursales en toda la ciudad.'),
 ('Descuentos para nuevos miembros', 'ActiveLife', '2023-03-10', '¡Únete a ActiveLife y disfruta de descuentos especiales como nuevo miembro!'),
@@ -162,19 +162,19 @@ VALUES
 INSERT INTO user (name, surname, username, password, address, type, status, gender, email, worker_id, dni, job_title_worker, phone_num, client_id, credit_card, birthday, url_img, plan_id) 
 VALUES
 ('John', 'Doe', 'john_doe', 'password123', '123 Main St', 1, 1, 'Male', 'john.doe@example.com', 101, '123456789', 'Manager', '555-1234', NULL, '1234-5678-9012-3456', '1990-01-15', 'http://example.com/johndoe.jpg', 1),
-('Alice', 'Smith', 'alice_smith', 'pass123', '456 Oak St', 2, 1, 'Female', 'alice.smith@example.com', NULL, '987654321', 'Developer', '555-5678', 201, '9876-5432-1098-7654', '1985-05-20', 'http://example.com/alicesmith.jpg', 2),
+('Alice', 'Smith', 'alice_smith', 'pass123', '456 Oak St', 2, 1, 'Female', 'alice.smith@example.com', NULL, '987654321', NULL, '555-5678', 201, '9876-5432-1098-7654', '1985-05-20', 'http://example.com/alicesmith.jpg', 2),
 ('Bob', 'Johnson', 'bob_johnson', 'securepass', '789 Pine St', 1, 0, 'Male', 'bob.johnson@example.com', 102, '112233445', 'Supervisor', '555-8765', NULL, '6543-2109-8765-4321', '1978-11-03', 'http://example.com/bobjohnson.jpg', 3),
-('Eva', 'Williams', 'eva_williams', 'eva_pass', '101 Cedar St', 2, 1, 'Female', 'eva.williams@example.com', NULL, '334455667', 'Coordinator', '555-4321', 203, '8765-4321-0987-5678', '1993-08-08', 'http://example.com/evawilliams.jpg', 1),
+('Eva', 'Williams', 'eva_williams', 'eva_pass', '101 Cedar St', 2, 1, 'Female', 'eva.williams@example.com', NULL, '334455667', NULL, '555-4321', 203, '8765-4321-0987-5678', '1993-08-08', 'http://example.com/evawilliams.jpg', 1),
 ('Michael', 'Lee', 'michael_lee', 'pass4321', '202 Elm St', 1, 1, 'Male', 'michael.lee@example.com', 103, '889900011', 'Analyst', '555-9876', NULL, '4321-0987-7654-3210', '1982-04-25', 'http://example.com/michaellee.jpg', 2),
-('Laura', 'Anderson', 'laura_anderson', 'laura_pass', '303 Maple St', 2, 1, 'Female', 'laura.anderson@example.com', NULL, '556677889', 'Trainer', '555-3456', 204, '7654-3210-9876-5432', '1995-02-12', 'http://example.com/lauraanderson.jpg', 3),
+('Laura', 'Anderson', 'laura_anderson', 'laura_pass', '303 Maple St', 2, 1, 'Female', 'laura.anderson@example.com', NULL, '556677889', NULL, '555-3456', 204, '7654-3210-9876-5432', '1995-02-12', 'http://example.com/lauraanderson.jpg', 3),
 ('Daniel', 'Brown', 'daniel_brown', 'passDaniel', '404 Birch St', 1, 0, 'Male', 'daniel.brown@example.com', 104, '1122334455', 'Technician', '555-6543', NULL, '8765-4321-2345-6789', '1989-07-30', 'http://example.com/danielbrown.jpg', 1),
-('Sophie', 'Clark', 'sophie_clark', 'sophie123', '505 Oak St', 2, 1, 'Female', 'sophie.clark@example.com', NULL, '223344556', 'Instructor', '555-7654', 205, '2345-6789-0123-4567', '1998-09-18', 'http://example.com/sophieclark.jpg', 2),
+('Sophie', 'Clark', 'sophie_clark', 'sophie123', '505 Oak St', 2, 1, 'Female', 'sophie.clark@example.com', NULL, '223344556', NULL, '555-7654', 205, '2345-6789-0123-4567', '1998-09-18', 'http://example.com/sophieclark.jpg', 2),
 ('Ryan', 'Evans', 'ryan_evans', 'ryan_pass', '606 Pine St', 1, 1, 'Male', 'ryan.evans@example.com', 105, '778899001', 'Assistant', '555-8765', NULL, '5678-9012-3456-7890', '1984-12-05', 'http://example.com/ryanevans.jpg', 3),
-('Emma', 'Fisher', 'emma_fisher', 'emma_pass', '707 Cedar St', 2, 1, 'Female', 'emma.fisher@example.com', NULL, '1122334455', 'Manager', '555-9876', 206, '8901-2345-6789-0123', '1990-06-23', 'http://example.com/emmafisher.jpg', 1),
+('Emma', 'Fisher', 'emma_fisher', 'emma_pass', '707 Cedar St', 2, 1, 'Female', 'emma.fisher@example.com', NULL, '1122334455', NULL, '555-9876', 206, '8901-2345-6789-0123', '1990-06-23', 'http://example.com/emmafisher.jpg', 1),
 ('David', 'Garcia', 'david_garcia', 'davidpass', '808 Elm St', 1, 1, 'Male', 'david.garcia@example.com', 106, '3344556677', 'Coordinator', '555-5432', NULL, '3456-7890-1234-5678', '1987-03-15', 'http://example.com/davidgarcia.jpg', 2),
-('Olivia', 'Hall', 'olivia_hall', 'passolivia', '909 Maple St', 2, 1, 'Female', 'olivia.hall@example.com', NULL, '8899001122', 'Analyst', '555-6543', 207, '6789-0123-4567-8901', '1992-10-08', 'http://example.com/oliviahall.jpg', 3),
+('Olivia', 'Hall', 'olivia_hall', 'passolivia', '909 Maple St', 2, 1, 'Female', 'olivia.hall@example.com', NULL, '8899001122', NULL, '555-6543', 207, '6789-0123-4567-8901', '1992-10-08', 'http://example.com/oliviahall.jpg', 3),
 ('Henry', 'Irwin', 'henry_irwin', 'henrypass', '101 Pine St', 1, 1, 'Male', 'henry.irwin@example.com', 107, '3344556677', 'Trainer', '555-7654', NULL, '1234-5678-9012-3456', '1986-06-20', 'http://example.com/henryirwin.jpg', 1),
-('Sophia', 'Johnson', 'sophia_johnson', 'sophiapass', '202 Oak St', 2, 1, 'Female', 'sophia.johnson@example.com', NULL, '1122334455', 'Instructor', '555-8765', 208, '4567-8901-2345-6789', '1994-04-02', 'http://example.com/sophiajohnson.jpg', 2),
+('Sophia', 'Johnson', 'sophia_johnson', 'sophiapass', '202 Oak St', 2, 1, 'Female', 'sophia.johnson@example.com', NULL, '1122334455', NULL, '555-8765', 208, '4567-8901-2345-6789', '1994-04-02', 'http://example.com/sophiajohnson.jpg', 2),
 ('Ethan', 'King', 'ethan_king', 'ethanpass', '303 Birch St', 1, 0, 'Male', 'ethan.king@example.com', 108, '2233445566', 'Assistant', '555-9876', NULL, '7890-1234-5678-9012', '1997-12-12', 'http://example.com/ethanking.jpg', 3);
 
 
