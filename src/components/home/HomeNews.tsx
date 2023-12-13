6949cc055675c7164418f45dd52a2311b58ff541
Susana Bergaz <susannabergaz@hotmail.com>
2023-12-13 13:52:59 +0100

    ALL_13_12_23: añadiendo endpoints en Plans y Home

--- src/components/home/HomeNews.tsx
+++ src/components/home/HomeNews.tsx
@@ -1,20 +1,29 @@
 import { New } from "./index";
-import Styles from "./styles.module.css"
-import dataNews from "../../data/news.json"
+import Styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
-type Props = {}
+export default function HomeNews() {
+  const [dataNews, setDataNews] = useState([]);
 
-export default function HomeNews({ }: Props) {
+  useEffect(() => {
+    fetch("http://localhost:8080/news", {
+      method: "GET",
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((result) => setDataNews(result))
+      .catch((error) => console.log("Error fetching news:", error));
+    }, []); 
+
     return (
-        <div className={Styles.estructure}>
-            <h1 className={Styles.title}>- NOVEDADES -</h1>
-            {dataNews.novedades.map((novedad) => {
-                const {id, titulo, gym_name, fecha, informacion } = novedad
-                return (
-                    <New key={id} title={titulo} gym_name={gym_name} date={fecha} info={informacion} />
-                )
-            })}
-            
-        </div>
-    )
+      <div className={Styles.estructure}>
+        <h1 className={Styles.title}>- NOVEDADES -</h1>
+        {dataNews.map((novedad) => {
+          const { id, titulo, gym_name, fecha, informacion } = novedad;
+          return (
+            <New key={id} title={titulo} gym_name={gym_name} date={fecha} info={informacion}/>
+          );
+        })}
+      </div>
+    );
 }
