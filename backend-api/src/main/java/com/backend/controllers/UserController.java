a1875d880e25e94b395a314762092a41f3eb6973
Frnkp21 <perezcarvajalfrank@elpuig.xeill.net>
2023-12-17 22:51:59 +0100

    endpoint modificarUser

--- backend-api/src/main/java/com/backend/controllers/UserController.java
+++ backend-api/src/main/java/com/backend/controllers/UserController.java
@@ -72,4 +72,17 @@ public class UserController {
 		return (result) ? ResponseEntity.status(HttpStatus.OK).body("Usuario eliminado")
 				: ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ese usuario");
 	}
+	
+	// Modifica un usuario
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateUser(@PathVariable Integer id, @RequestBody UserDto updatedUserData) {
+	    if (!userService.existsUser(id)) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ese usuario");
+	    }
+	    boolean result = userService.updateUser(id, updatedUserData);
+
+	    return (result) ? ResponseEntity.status(HttpStatus.OK).body("Usuario modificado exitosamente")
+	            : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al modificar el usuario");
+	}
+
 }
